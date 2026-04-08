8f5183fb4b424aaa075f401eb277ee89e3cfc9b8
Baron Liu <baronliu1993@gmail.com>
2026-04-08 19:46:04 -0400

    app sidebar and auth

---
+++ app/custom/sidebar/appSidebar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Home, Bookmark, TrendingUp, Settings, List, Film } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const menuItems = [
+  { title: "Home", icon: <Home className="!size-8 stroke-[1.5px]" />, href: "/home" },
+  { title: "Trending", icon: <Film className="!size-8 stroke-[1.5px]" />, href: "/trending" },
+  { title: "Bookmarks", icon: <Bookmark className="!size-8 stroke-[1.5px]" />, href: "/bookmarks" },
+  { title: "Settings", icon: <Settings className="!size-8 stroke-[1.5px]" />, href: "/settings" },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menuItems.map((item) => (
+                <SidebarMenuItem key={item.title} className = "py-5">
+                  <SidebarMenuButton asChild>
+                    <a className = "h-10 w-10" href={item.href}>
+                      {item.icon}
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
